0a7e67a565822851df408a5ec608b55240499c32
rocketpowers <rocketpowerswillian@gmail.com>
2022-03-08 22:40:06 -0300

    project created

---
+++ backend/src/main/java/com/packages/dsmoovie/DsmoovieApplication.java
@@ -0,0 +1,13 @@
+package com.packages.dsmoovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/packages/dsmoovie/DsmoovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packages.dsmoovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
