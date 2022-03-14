c30a8a3f73113fcf58d7393ae1b63d661d79a261
rocketpowers <rocketpowerswillian@gmail.com>
2022-03-14 00:34:02 -0300

    userEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,14 +2,29 @@ import Pagination from "components/Pagination";
 import MoovieCard from "components/MoovieCard";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MooviePage } from "types/moovies";
 
 function Listing() {
 
+const [pageNumber, setPageNumber]= useState(0);
+
+useEffect(()=>{
+
   axios.get(`${BASE_URL}/moovies`).then(Response=>{
-    console.log(Response.data);
+    const data = Response.data as MooviePage;
+    console.log(data);
+    setPageNumber(data.number);
   });
+
+}, []);
+
+
+
   return (
     <>
+
+    <p> {pageNumber} </p>
       <Pagination />
 
       <div className="container">
